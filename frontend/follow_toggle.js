23d5b66eafc98370b2cc90006cdfbf7098d25584
Tu Ngo <tungo8810@gmail.com>
2017-05-03 14:08:32 -0700

    Add promises and disabled buttons to follow button

--- frontend/follow_toggle.js
+++ frontend/follow_toggle.js
@@ -1,3 +1,5 @@
+const APIUtil = require('./api_util.js');
+
 class FollowToggle {
   constructor(el) {
     this.$el = $(el);
@@ -13,28 +15,42 @@ class FollowToggle {
   render() {
     if (this.followState === 'unfollowed') {
       this.$el.text('Follow!');
+      this.$el.prop('disabled', false);
     } else if (this.followState === 'followed') {
       this.$el.text('Unfollow!');
+      this.$el.prop('disabled', false);
+    } else if (this.followState === 'following') {
+      this.$el.prop('disabled', true);
+    } else if (this.followState === 'unfollowing') {
+      this.$el.prop('disabled', true);
     }
   }
 
   handleClick(e) {
     e.preventDefault();
-    $.ajax({
-      method: (this.followState === 'followed') ? 'DELETE' : 'POST',
-      url: `/users/${this.userId}/follow`,
-      dataType: 'json',
-      success: (res) => {
-        this.toggleFollowState();
-        this.render();
-      }
+
+
+    let promise;
+    if (this.followState === 'unfollowed') {
+      this.followState = 'following';
+      this.render();
+      promise = APIUtil.followUser(this.userId);
+    } else if (this.followState === 'followed') {
+      this.followState = 'unfollowing';
+      this.render();
+      promise = APIUtil.unfollowUser(this.userId);
+    }
+
+    promise.then(() => {
+      this.toggleFollowState();
+      this.render();
     });
   }
 
   toggleFollowState() {
-    if (this.followState === 'unfollowed') {
+    if (this.followState === 'unfollowed' || this.followState === 'following') {
       this.followState = 'followed';
-    } else if (this.followState === 'followed') {
+    } else if (this.followState === 'followed' || this.followState === 'unfollowing') {
       this.followState = 'unfollowed';
     }
   }
